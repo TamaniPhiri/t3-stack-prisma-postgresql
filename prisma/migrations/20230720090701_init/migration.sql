ed101b0916d0d2c75d3d834e3b99fce424d3aacd
Tamani Phiri <tamanigabriel0@gmail.com>
2023-07-20 13:32:04 +0200

    routes complete

---
+++ prisma/migrations/20230720090701_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
+);
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,15 +1,63 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const idSchema=z.object({id:z.string()});
+
+const userSchema=z.object({
+  name:z.string(),
+  email:z.string(),
+});
+
+const userUpdateSchema=z.object({
+  name:z.string(),
+  email:z.string(),
+  id:z.string(),
+});
+
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+  //get all users
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return ctx.prisma.client.findMany()
   }),
+
+  //get user by id
+  getOne: publicProcedure
+    .input(idSchema)
+    .query(({input,ctx }) => {
+      return ctx.prisma.client.findUnique({
+        where:idSchema.parse(input)
+      })
+    }),
+
+    //create a new user
+    createUser:publicProcedure
+    .input(userSchema)
+    .mutation(({input,ctx}) => {
+      return ctx.prisma.client.create({
+        data:userSchema.parse(input)
+      });
+    }),
+
+    //update a user
+    updateUser:publicProcedure
+    .input(userUpdateSchema)
+    .mutation(({input,ctx}) => {
+      return ctx.prisma.client.update({
+        where:{
+          id:input.id.toString()
+        },
+        data:userUpdateSchema.parse(input)
+      });
+    }),
+
+    //delete a user
+    deleteUser:publicProcedure
+    .input(idSchema)
+    .mutation(({input,ctx}) => {
+      return ctx.prisma.client.delete({
+        where: {
+          id:idSchema.parse(input)
+        }
+      });
+    }),
 });
